4ff8b90f93f168ec52aada7bc02f60c0036f10a9
Boris Klymko <boris.klymko@gmail.com>
2017-03-17 19:16:49 +0100

    iptables-init moved in /etc before use

--- initvm/0_init.sh
+++ initvm/0_init.sh
@@ -2,8 +2,9 @@
 
 export PATH=$PATH:/sbin:/usr/sbin:/bin
 
-chmod 700 "$(dirname "$0")/iptables-init/init"
-"$(dirname "$0")/iptables-init/init" --quiet start
+mv "$(dirname "$0")/iptables-init" /etc/
+chmod 700 /etc/iptables-init/init
+/etc/iptables-init/init --quiet start
 
 # mkpasswd --method=sha-512 the-password
 usermod -aG sudo -md /home/myuser -l myuser --password '$6$2AmnAKMiIhOt3sY6$N.v3igMQHu9ImrkHMz/N4j5EIeb3ETSZn8l.vpT.nRL2EtEN.lPqDJzoA/gOmpa8R7y0t1h5.isxFqrJRDjrq.' debian
